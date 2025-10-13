49057e58c2ca505a4d0b41cd622580960f34ea60
Loukraasz <peixotolukas06@gmail.com>
2025-10-13 20:07:16 -0300

    ...

--- Portfolio/Portfolio/urls.py
+++ Portfolio/Portfolio/urls.py
@@ -6,7 +6,7 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
+    path('', views.index, name='index',),
     distill_path(
         '', 
         views.index, 
